2545a4918af2451ccfad2bb4e805aef4ce2f23d5
rlplsj <sandeep.jidagi@relevancelab.com>
2026-04-13 14:44:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(f":cup_with_straw: Customize your Smoothie :cup_with_straw:")
+st.title(f":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
 st.write(
   """Choose the fruits you want in your custom smoothie!
   """
