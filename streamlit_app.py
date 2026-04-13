da367e8b4e5f22378e1ab87006125dcc014ca4e5
rlplsj <sandeep.jidagi@relevancelab.com>
2026-04-13 15:58:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ try:
 
     # Retrieve fruit options from Snowflake
     my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col("SEARCH_ON"))
-
+    pd_df=my_dataframe.toPandas()
     # Multi-select for choosing ingredients
     ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 
@@ -32,6 +32,10 @@ try:
         for fruit_chosen in ingredients_list:
             try:
                 # Make API request to get details about each fruit
+                ingredients_string+=fruit_chosen + ''
+                search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+                st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+                
                 fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
                 fruityvice_response.raise_for_status()  # Raise an error for bad responses (4xx or 5xx)
                 
